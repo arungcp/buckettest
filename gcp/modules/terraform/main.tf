cf48c1fb30269101f3b7d342584939dd1b0c62a3
arungcp <arunkumar.veerapandian@gmail.com>
2025-01-22 17:00:33 +0530

    commit 5

--- gcp/modules/terraform/main.tf
+++ gcp/modules/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "auto-expire" {
   name          = var.name
-  location      = "US"
+  location      = var.location
   force_destroy = true
 
   public_access_prevention = "enforced"
