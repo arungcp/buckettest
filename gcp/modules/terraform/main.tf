3bcd605c58cc34746ca6015743d18c56f7aafe50
arungcp <arunkumar.veerapandian@gmail.com>
2025-01-23 20:26:07 +0530

    vpc

--- gcp/modules/terraform/main.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "auto-expire" {
-  name                     = var.name
-  location                 = var.location
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
---
+++ gcp/modules/terraform/network/main.tf
@@ -0,0 +1,3 @@
+resource "google_compute_network" "test_vpc" {
+  name = var.name
+}
